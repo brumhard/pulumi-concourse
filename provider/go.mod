339f01f11e61b47d0203d6fc3e40bd60a9532657
brumhard <fobytobi@gmail.com>
2021-09-10 18:10:43 +0200

    Add concourse client, add provider section to schema.json

--- provider/go.mod
+++ provider/go.mod
@@ -3,8 +3,18 @@ module github.com/brumhard/pulumi-concourse/provider
 go 1.16
 
 require (
+	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+	github.com/concourse/atc v4.2.2+incompatible // indirect
+	github.com/concourse/go-concourse v4.2.2+incompatible // indirect
 	github.com/golang/protobuf v1.5.2
+	github.com/google/jsonapi v1.0.0 // indirect
+	github.com/peterhellberg/link v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pulumi/pulumi/pkg/v3 v3.9.1
 	github.com/pulumi/pulumi/sdk/v3 v3.9.1
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/tedsuo/rata v1.0.0 // indirect
+	github.com/vito/go-sse v1.0.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
 	google.golang.org/grpc v1.37.0
 )
---
+++ provider/pkg/provider/client.go
@@ -0,0 +1,43 @@
+package provider
+
+import (
+	"context"
+	"github.com/concourse/go-concourse/concourse"
+	"github.com/pkg/errors"
+	"golang.org/x/oauth2"
+	"net/http"
+)
+
+var ErrUsernamePasswordRequired = errors.New("username and password are required")
+
+func newPasswordGrantHTTPClient(url, username, password string) (*http.Client, error) {
+	if username == "" || password == "" {
+		return nil, ErrUsernamePasswordRequired
+	}
+
+	oauth2Config := oauth2.Config{
+		ClientID:     "fly",
+		ClientSecret: "Zmx5",
+		Endpoint:     oauth2.Endpoint{TokenURL: url + "/sky/issuer/token"},
+		Scopes:       []string{"openid", "profile", "email", "federated:id", "groups"},
+	}
+
+	// TODO: set proper default http.Client
+	ctx := context.WithValue(context.Background(), oauth2.HTTPClient, http.Client{})
+
+	token, err := oauth2Config.PasswordCredentialsToken(ctx, username, password)
+	if err != nil {
+		return nil, err
+	}
+
+	return oauth2Config.Client(ctx, token), nil
+}
+
+func NewClient(url, username, password string) (concourse.Client, error) {
+	httpClient, err := newPasswordGrantHTTPClient(url, username, password)
+	if err != nil {
+		return nil, err
+	}
+
+	return concourse.NewClient(url, httpClient, false), nil
+}
